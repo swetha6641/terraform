96b59c4eb1a5028b984b3f432aeb5ea4928e3db8
swetha <pswetha060@gmail.com>
2025-10-01 09:01:13 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,3 +1,10 @@
 locals {
   fina_name = "${var.project}-${var.environment}-${var.component}"
+  ec2_tags = merge(
+    var.common_tags,
+    {
+        environment = "dev",
+        version = "1.0"
+    }
+  )
 }
--- locals/variables.tf
+++ locals/variables.tf
@@ -1,5 +1,5 @@
 variable "project" {
-    default = "robo"
+    default = "roboshop"
 }
 
 variable "environment" {
@@ -10,8 +10,16 @@ variable "component" {
     default = "cart"
 }
 
-/*variable "final-name"{
-    defa
-}*/
+variable "common_tags"{
+    default = {
+        Project = "roboshop"
+        Terraform = "true"
+    }
+}
 
+/* variable "final-name" {
+    default = "${var.project}-${var.environment}-${var.component}"
+} */
 
+# ec2 name = cart
+# ec2 name = roboshop-dev-cart
